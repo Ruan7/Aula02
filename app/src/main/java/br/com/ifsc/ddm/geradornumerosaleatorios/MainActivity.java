2aee05424977b60e10dbdc8d6043967334429542
aluno <rjlana43@hotmail.com>
2019-02-22 21:13:48 -0300

    Projeto gerador de números aleatórios criado com template Empity Activity

--- app/src/main/java/br/com/ifsc/ddm/geradornumerosaleatorios/MainActivity.java
+++ app/src/main/java/br/com/ifsc/ddm/geradornumerosaleatorios/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.ifsc.ddm.geradornumerosaleatorios;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void alteraTexto(View view) {
+
+       TextView text = (TextView) findViewById(R.id.text);
+        TextView texto = findViewById(R.id.editText2);
+
+        text.setText(texto.getText());
     }
 }
